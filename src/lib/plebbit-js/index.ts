a0d0b1b8f21798c27c00874527e08f35974c7105
Esteban Abaroa <estebanabaroa@protonmail.com>
2022-03-18 17:40:13 +0000

    mock content

--- src/lib/plebbit-js/index.ts
+++ src/lib/plebbit-js/index.ts
@@ -1,5 +1,6 @@
 // NOTE: don't import plebbit-js directly to be able to mock it for unit tests
 
+import PlebbitJsMockContent from './plebbit-js-mock-content'
 function Plebbit(plebbitOptions: any) {} // should import from plebbit-js npm module later instead
 
 const PlebbitJs = {
@@ -9,6 +10,11 @@ const PlebbitJs = {
 // mock the plebbit-js module for unit tests
 export function mockPlebbitJs(_Plebbit: any) {
   PlebbitJs.Plebbit = _Plebbit
+}
+
+// mock content for front-end dev with this env var
+if (process.env.REACT_APP_PLEBBIT_REACT_HOOKS_MOCK_CONTENT) {
+  mockPlebbitJs(PlebbitJsMockContent)
 }
 
 export default PlebbitJs
