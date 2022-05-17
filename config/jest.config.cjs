8878779632aaf556f3075129204f54505211e5c3
Esteban Abaroa <estebanabaroa@protonmail.com>
2022-05-17 12:58:11 +0000

    fix jest paths

--- config/jest.config.cjs
+++ config/jest.config.cjs
@@ -1,8 +1,16 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
-  rootDir: '..',
-  testPathIgnorePatterns: ['dist'],
+  rootDir: '../src',
+
+  /* no need for ignore when only testing rootDir ../src
+     might change later to include some tests in ../test
+  testPathIgnorePatterns: [
+    '<rootDir>/dist', 
+    // e2e tests are run in the browser with karma
+    // not with jest and jsdom
+    '<rootDir>/test/e2e/'], */
+
   globals: {
     'ts-jest': {
       tsconfig: 'config/tsconfig.json',
