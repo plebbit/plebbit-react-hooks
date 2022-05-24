b70c7812801e135fe5fc6546292b52326dd4231d
Esteban Abaroa <estebanabaroa@protonmail.com>
2022-05-24 23:15:40 +0000

    missing build files

---
+++ dist/hooks/utils/use-interval.js
@@ -0,0 +1,36 @@
+/* usage:
+
+useInterval(
+  callback: () => void,
+  delay: number,
+  immediate?: boolean
+)
+
+*/
+import { useEffect, useRef } from 'react';
+/** keep typescript happy */
+const noop = () => { };
+export function useInterval(callback, delay, immediate) {
+    const savedCallback = useRef(noop);
+    // Remember the latest callback.
+    useEffect(() => {
+        savedCallback.current = callback;
+    });
+    // Execute callback if immediate is set.
+    useEffect(() => {
+        if (!immediate)
+            return;
+        if (delay === null || delay === false)
+            return;
+        savedCallback.current();
+    }, [immediate]);
+    // Set up the interval.
+    useEffect(() => {
+        if (delay === null || delay === false)
+            return undefined;
+        const tick = () => savedCallback.current();
+        const id = setInterval(tick, delay);
+        return () => clearInterval(id);
+    }, [delay]);
+}
+export default useInterval;
