d05eb3df6a145723bcb5db3b8301ac6d45bc0fca
Esteban Abaroa <estebanabaroa@protonmail.com>
2022-07-14 04:19:58 +0000

    fix scripts

--- scripts/build-watch-all.sh
+++ scripts/build-watch-all.sh
@@ -2,4 +2,5 @@
 
 xterm -geometry "-0+0" -e "yarn build:watch" &
 xterm -geometry "+0+0" -e "yarn webpack:watch" &
-xterm -geometry "-0-0" -e "yarn test:server"
+xterm -geometry "-0-0" -e "yarn test:server" &
+sleep infinity
