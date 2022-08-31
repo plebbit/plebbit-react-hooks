08618556fd13c87bc61a549f86b839df7ab75e8d
Esteban Abaroa <estebanabaroa@protonmail.com>
2022-08-31 04:48:55 +0000

    add subplebbit to error log

--- src/stores/feeds/feeds-store.ts
+++ src/stores/feeds/feeds-store.ts
@@ -241,7 +241,7 @@ const addSubplebbitsPagesOnLowBufferedFeedsSubplebbitsPostCounts = (feedsStoreSt
       // subplebbit post count is low, fetch next subplebbit page
       if (subplebbitsPostCounts[subplebbitAddress] <= subplebbitPostsLeftBeforeNextPage) {
         addNextSubplebbitPageToStore(subplebbits[subplebbitAddress], sortType, account).catch((error: unknown) =>
-          log.error('feedsStore subplebbitsActions.addNextSubplebbitPageToStore error', {subplebbitAddress, sortType, error})
+          log.error('feedsStore subplebbitsActions.addNextSubplebbitPageToStore error', {subplebbitAddress, subplebbit: subplebbits[subplebbitAddress], sortType, error})
         )
       }
     }
