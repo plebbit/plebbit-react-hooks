704ab1257b498ebd5cb227be92eb81be0fca7f67
Esteban Abaroa <estebanabaroa@protonmail.com>
2023-01-24 00:10:20 +0000

    fix typo

--- src/stores/accounts/accounts-actions.ts
+++ src/stores/accounts/accounts-actions.ts
@@ -115,7 +115,7 @@ export const setAccount = async (account: Account) => {
   // add subplebbit roles to account if account.author.address has changed
   // TODO: add test to check if roles get added
   if (accounts[account.id].author.address !== newAccounts[account.id].author.address) {
-    const subplebbits: Subplebbits = subplebbitsStore.getState()
+    const subplebbits: Subplebbits = subplebbitsStore.getState().subplebbits
     for (const subplebbitAddress in subplebbits) {
       accountsActionsInternal.addSubplebbitRoleToAccountsSubplebbits(subplebbits[subplebbitAddress])
     }
@@ -164,7 +164,7 @@ export const importAccount = async (serializedAccount: string) => {
 
   // add subplebbit roles to imported account
   // TODO: add test to check if roles get added
-  const subplebbits: Subplebbits = subplebbitsStore.getState()
+  const subplebbits: Subplebbits = subplebbitsStore.getState().subplebbits
   for (const subplebbitAddress in subplebbits) {
     accountsActionsInternal.addSubplebbitRoleToAccountsSubplebbits(subplebbits[subplebbitAddress])
   }
