5fa7da2bd995a0e0364021079108cf43028079ec
estebanabaroa <estebanabaroa@users.noreply.github.com>
2023-01-28 22:03:22 +0000

    CI build

--- dist/lib/utils/utils.d.ts
+++ dist/lib/utils/utils.d.ts
@@ -3,9 +3,9 @@ declare const utils: {
     merge: (...args: any) => any;
     clone: (obj: any) => any;
     flattenCommentsPages: (pageInstanceOrPagesInstance: any) => any[];
-    retryInfinity: (f: any) => Promise<unknown>;
+    retryInfinity: (f: any) => any;
     retryInfinityMinTimeout: number;
     retryInfinityMaxTimeout: number;
 };
-export declare const retryInfinity: (functionToRetry: any) => Promise<unknown>;
+export declare const retryInfinity: (functionToRetry: any) => Promise<any>;
 export default utils;
