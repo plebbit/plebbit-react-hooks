ecc42fe4fa2c4a76d14905e9e6097fe35dae5994
estebanabaroa <estebanabaroa@users.noreply.github.com>
2023-01-28 23:25:10 +0000

    CI build

---
+++ dist/stores/accounts/utils.d.ts
@@ -0,0 +1,6 @@
+import { Account, Subplebbits } from '../../types';
+export declare const getAccountSubplebbits: (account: Account, subplebbits: Subplebbits) => any;
+declare const utils: {
+    getAccountSubplebbits: (account: Account, subplebbits: Subplebbits) => any;
+};
+export default utils;
