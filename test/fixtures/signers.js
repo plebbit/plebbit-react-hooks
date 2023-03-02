718e451c025005b1e5c3b7c99394dc62dbdc5a23
Rinse <rinse12@tutanota.com>
2023-03-02 07:19:27 +0000

    update signers

--- test/fixtures/signers.js
+++ test/fixtures/signers.js
@@ -1,8 +1,8 @@
 const signers = []
 
 signers[0] = {
-  privateKey: `TnH1eAqiWEhEghGb1hyCdZwhiMGeN5PLRZSbjBh0DlQ`,
-  address: '12D3KooWANwdyPERMQaCgiMnTT1t3Lr4XLFbK1z4ptFVhW2ozg1z',
+  privateKey: `8rHln2/6BDnbNoyd9PgjDJr27tuP6N2PfWIdh+EXcHs`,
+  address: '12D3KooWMZPQsQdYtrakc4D1XtzGXwN1X3DBnAobcCjcPYYXTB6o',
 }
 
 module.exports = signers
