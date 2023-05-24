90ae57e9b8e633964352719e7984b38c2291ef67
Rinse <rinse12@tutanota.com>
2023-05-24 07:33:43 +0000

    change plebbit data path to /tmp for electron. May eliminate SQLite IO errors

--- config/karma-electron.conf.js
+++ config/karma-electron.conf.js
@@ -44,7 +44,7 @@ console.log('before electron preload.js contextBridge.exposeInMainWorld plebbitJ
 const {contextBridge} = require('electron')
 const path = require ('path')
 contextBridge.exposeInMainWorld('plebbitJsNativeFunctions', require('@plebbit/plebbit-js').nativeFunctions.node)
-contextBridge.exposeInMainWorld('plebbitDataPath', path.join(process.cwd(), '.plebbit'))
+contextBridge.exposeInMainWorld('plebbitDataPath', /tmp/.plebbit)
 console.log('after electron preload.js contextBridge.exposeInMainWorld plebbitJsNativeFunctions')
 `
 const preloadJsPath = path.resolve(__dirname, '..', 'karma-electron-preload.js')
