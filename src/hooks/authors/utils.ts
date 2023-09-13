1730c8ca7708df60df6b5a8c30ed62bace893ff8
Esteban Abaroa <estebanabaroa@protonmail.com>
2023-09-13 03:14:32 +0000

    get plebbit address sync

--- src/hooks/authors/utils.ts
+++ src/hooks/authors/utils.ts
@@ -5,8 +5,7 @@ import utils from '../../lib/utils'
 import PeerId from 'peer-id'
 import {fromString as uint8ArrayFromString} from 'uint8arrays/from-string'
 import {toString as uint8ArrayToString} from 'uint8arrays/to-string'
-// @ts-ignore
-import {Ed25519PublicKey, Ed25519PrivateKey} from 'libp2p-crypto/src/keys/ed25519-class'
+import {create as createMultihash} from 'multiformats/hashes/digest'
 
 // filters are functions so they can't be stringified
 const filterNumbers = new WeakMap()
@@ -26,29 +25,17 @@ export const useAuthorCommentsName = (accountId?: string, authorAddress?: string
   return useMemo(() => accountId + '-' + authorAddress + '-' + filterName, [accountId, authorAddress, filterName])
 }
 
-const getPeerIdFromPublicKey = async (publicKeyBase64: string) => {
+const protobufPublicKeyPrefix = new Uint8Array([8, 1, 18, 32])
+const multihashIdentityCode = 0
+const getPlebbitAddressFromPublicKey = (publicKeyBase64: string) => {
   const publicKeyBuffer = uint8ArrayFromString(publicKeyBase64, 'base64')
-
-  // the PeerId public key is not a raw public key, it adds a suffix
-  const ed25519PublicKeyInstance = new Ed25519PublicKey(publicKeyBuffer)
-  const peerId = await PeerId.createFromPubKey(new Uint8Array(ed25519PublicKeyInstance.bytes)) // add new Uint8Array or bugs out in jsdom
-  return peerId
-}
-
-const getPlebbitAddressFromPublicKey = async (publicKeyBase64: string) => {
-  const peerId = await getPeerIdFromPublicKey(publicKeyBase64)
-  return peerId.toB58String().trim()
+  const publicKeyBufferWithPrefix = new Uint8Array(protobufPublicKeyPrefix.length + publicKeyBuffer.length)
+  publicKeyBufferWithPrefix.set(protobufPublicKeyPrefix, 0)
+  publicKeyBufferWithPrefix.set(publicKeyBuffer, protobufPublicKeyPrefix.length)
+  const multihash = createMultihash(multihashIdentityCode, publicKeyBufferWithPrefix).bytes
+  return uint8ArrayToString(multihash, 'base58btc')
 }
 
 export const usePlebbitAddress = (publicKeyBase64: string) => {
-  const [plebbitAddress, setPlebbitAddress] = useState<string>()
-  useEffect(() => {
-    if (typeof publicKeyBase64 !== 'string') {
-      return
-    }
-    getPlebbitAddressFromPublicKey(publicKeyBase64)
-      .then((plebbitAddress) => setPlebbitAddress(plebbitAddress))
-      .catch(() => {})
-  }, [publicKeyBase64])
-  return plebbitAddress
+  return useMemo(() => getPlebbitAddressFromPublicKey(publicKeyBase64), [publicKeyBase64])
 }
