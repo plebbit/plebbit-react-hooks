b74cc63f9622fb4d0d52cb2e30c321d6aff955c2
Esteban Abaroa <estebanabaroa@protonmail.com>
2023-09-23 21:19:42 +0000

    fix(utils): incorrect import

--- src/hooks/utils/use-previous.ts
+++ src/hooks/utils/use-previous.ts
@@ -1,6 +1,8 @@
+import {useState} from 'react'
+
 export default function usePrevious<Type>(value: Type) {
-  const [current, setCurrent] = React.useState<Type>(value)
-  const [previous, setPrevious] = React.useState<Type>()
+  const [current, setCurrent] = useState<Type>(value)
+  const [previous, setPrevious] = useState<Type>()
 
   if (value !== current) {
     setPrevious(current)
