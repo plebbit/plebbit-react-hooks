85b03d3fcacb934b11c8dacae3185b519632311e
Rinse <rinse12@tutanota.com>
2024-02-12 16:03:39 +0000

    build(esm): latest work in migrating to jest ESM. Fails to load browser files

--- config/jest.config.ts
+++ config/jest.config.ts
@@ -1,35 +1,23 @@
 import type {JestConfigWithTsJest} from 'ts-jest'
+
 const config: JestConfigWithTsJest = {
-  preset: 'ts-jest/presets/default-esm',
   verbose: true,
-  resolver: '../config/jest.resolver.cjs', // need to use custom resolver cause otherwise it would use dist/node
-  // resolver: "enhanced-resolve-jest",
-  // "resolver": "@financial-times/jest-browser-resolver",
   testEnvironment: 'jsdom',
   rootDir: '../src',
   setupFilesAfterEnv: ['../config/jest.setup.cjs'],
-  transformIgnorePatterns: ['node_modules/(?!(uuid|uint8arrays|@plebbit/plebbit-js)/)'],
-  extensionsToTreatAsEsm: ['.ts'],
-
-  /* no need for ignore when only testing rootDir ../src
-     might change later to include some tests in ../test
-  testPathIgnorePatterns: [
-    '<rootDir>/dist', 
-    // e2e tests are run in the browser with karma
-    // not with jest and jsdom
-    '<rootDir>/test/e2e/'], */
-
-  // transform: {},
+  moduleNameMapper: {
+    '^(\\.{1,2}/.*)\\.js$': '$1',
+    uuid: require.resolve('uuid'), // Force module uuid to resolve with the CJS entry point, because Jest does not support package.json.exports. See https://github.com/uuidjs/uuid/issues/451
+  },
+  extensionsToTreatAsEsm: ['.jsx', '.ts', '.tsx', '.mts'],
   transform: {
     // '^.+\\.[tj]sx?$' to process js/ts with `ts-jest`
     // '^.+\\.m?[tj]sx?$' to process js/ts/mjs/mts with `ts-jest`
-    '^.+\\.(j|t)sx?$': [
+    '^.+\\.[tj]sx?$': [
       'ts-jest',
       {
-        tsconfig: 'config/tsconfig.json',
+        tsconfig: 'config/jest.tsconfig.json',
         useESM: true,
-        allowJs: true,
-        supportsStaticESM: true,
       },
     ],
   },
