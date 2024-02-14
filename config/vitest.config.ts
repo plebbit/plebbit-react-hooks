c73e07e012514db9bc58c031269298ac731e6fae
Rinse <rinse12@tutanota.com>
2024-02-14 08:32:44 +0000

    test(vitest): use jsdom in vitest now, make sure it uses web crypto API

--- config/vitest.config.ts
+++ config/vitest.config.ts
@@ -4,12 +4,10 @@ import {defineConfig} from 'vitest/config'
 export default defineConfig({
   test: {
     name: 'test src files',
-    environment: 'happy-dom',
+    environment: 'jsdom',
     root: 'src/',
     setupFiles: ['../config/vitest.setup.cjs'],
     // retry:  10,
     // testTimeout: 60000
-
-    // ...
   },
 })
--- config/vitest.setup.cjs
+++ config/vitest.setup.cjs
@@ -4,3 +4,4 @@
 const {TextEncoder, TextDecoder} = require('util')
 global.TextEncoder = TextEncoder
 global.TextDecoder = TextDecoder
+global.crypto = undefined // You have to make crypto undefined so it uses web API
