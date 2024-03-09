0e1404c6389e1be089b2bdbf941abcc82c05ea85
Rinse <rinse12@tutanota.com>
2024-03-09 07:50:07 +0000

    handle weird import error with solana library

--- config/vitest.config.ts
+++ config/vitest.config.ts
@@ -5,6 +5,14 @@ export default defineConfig({
   test: {
     globals: true,
     environment: 'jsdom',
+    server: {
+      deps: {
+        inline: true,
+      }
+    },
+    alias: {
+      "@solana/web3.js": "@solana/web3.js/lib/index.browser.esm.js"
+    },
     root: 'src/',
     setupFiles: ['../config/vitest.setup.js'],
   },
