d3d98f4e5612983e38ccb465af59e4d87123d8a4
Esteban Abaroa <estebanabaroa@protonmail.com>
2025-02-27 02:28:04 +0000

    feat(replies-pages-store): implement replies pages store

---
+++ src/stores/replies-pages/index.ts
@@ -0,0 +1,3 @@
+import repliesPagesStore from './replies-pages-store'
+export * from './replies-pages-store'
+export default repliesPagesStore
---
+++ src/stores/replies-pages/replies-pages-store.ts
@@ -0,0 +1,263 @@
+import utils from '../../lib/utils'
+import Logger from '@plebbit/plebbit-logger'
+// include repliess pages store with feeds for debugging
+const log = Logger('plebbit-react-hooks:replies:stores')
+import {Subplebbit, SubplebbitPage, RepliesPages, Account, Comment, Comments} from '../../types'
+import accountsStore from '../accounts'
+import subplebbitsStore, {SubplebbitsState} from '../subplebbits'
+import localForageLru from '../../lib/localforage-lru'
+import createStore from 'zustand'
+import assert from 'assert'
+
+const repliesPagesDatabase = localForageLru.createInstance({name: 'repliesPages', size: 500})
+
+// reset all event listeners in between tests
+export const listeners: any = []
+
+export type RepliesPagesState = {
+  repliesPages: RepliesPages
+  comments: Comments
+  addNextRepliesPageToStore: Function
+  addRepliesPageCommentsToStore: Function
+}
+
+const repliesPagesStore = createStore<RepliesPagesState>((setState: Function, getState: Function) => ({
+  // TODO: eventually clear old pages and comments from memory
+  repliesPages: {},
+  comments: {},
+
+  addNextRepliesPageToStore: async (comment: Comment, sortType: string, account: Account) => {
+    assert(comment?.cid && typeof comment?.cid === 'string', `repliesPagesStore.addNextRepliesPageToStore comment '${comment}' invalid`)
+    assert(sortType && typeof sortType === 'string', `repliesPagesStore.addNextRepliesPageToStore sortType '${sortType}' invalid`)
+    assert(typeof account?.plebbit?.createSubplebbit === 'function', `repliesPagesStore.addNextRepliesPageToStore account '${account}' invalid`)
+
+    // check the preloaded posts on comment.replies.pages first, then the comment.replies.pageCids
+    const repliesFirstPageCid = getRepliesFirstPageCid(comment, sortType)
+    assert(
+      repliesFirstPageCid && typeof repliesFirstPageCid === 'string',
+      `repliesPagesStore.addNextRepliesPageToStore comment '${comment?.cid}' sortType '${sortType}' repliesFirstPageCid '${repliesFirstPageCid}' invalid`
+    )
+
+    // all replies pages in store
+    const repliesPagesStore = getState()
+    // only specific pages of the comment+sortType
+    const repliesPages = getRepliesPages(comment, sortType, repliesPagesStore.repliesPages)
+
+    // if no pages exist yet, add the first page
+    let pageCidToAdd: string
+    if (!repliesPages.length) {
+      pageCidToAdd = repliesFirstPageCid
+    } else {
+      const nextCid = repliesPages[repliesPages.length - 1]?.nextCid
+      // if last nextCid is undefined, reached end of pages
+      if (!nextCid) {
+        log.trace('repliesPagesStore.addNextRepliesPageToStore no more pages', {commentCid: comment.cid, sortType, account})
+        return
+      }
+
+      pageCidToAdd = nextCid
+    }
+
+    // page is already added or pending
+    if (repliesPages[pageCidToAdd] || fetchPagePending[account.id + pageCidToAdd]) {
+      return
+    }
+
+    fetchPagePending[account.id + pageCidToAdd] = true
+    let page: SubplebbitPage
+    try {
+      page = await fetchPage(pageCidToAdd, comment.subplebbitAddress, account)
+      log.trace('repliesPagesStore.addNextRepliesPageToStore comment.replies.getPage', {
+        pageCid: pageCidToAdd,
+        commentCid: comment.cid,
+        subplebbitAddress: comment.subplebbitAddress,
+        account,
+      })
+    } catch (e) {
+      throw e
+    } finally {
+      fetchPagePending[account.id + pageCidToAdd] = false
+    }
+
+    // failed getting the page
+    if (!page) {
+      return
+    }
+
+    // find new comments in the page
+    const flattenedComments = utils.flattenCommentsPages(page)
+    const {comments} = getState()
+    let hasNewComments = false
+    const newComments: Comments = {}
+    for (const comment of flattenedComments) {
+      if (comment.cid && (comment.updatedAt || 0) > (comments[comment.cid]?.updatedAt || 0)) {
+        // don't clone the comment to save memory, comments remain a pointer to the page object
+        newComments[comment.cid] = comment
+        hasNewComments = true
+      }
+    }
+
+    setState(({repliesPages, comments}: any) => {
+      const newState: any = {repliesPages: {...repliesPages, [pageCidToAdd]: page}}
+      if (hasNewComments) {
+        newState.comments = {...comments, ...newComments}
+      }
+      return newState
+    })
+    log('repliesPagesStore.addNextRepliesPageToStore', {pageCid: pageCidToAdd, commentCid: comment.cid, sortType, page, account})
+
+    // when publishing a comment, you don't yet know its CID
+    // so when a new comment is fetched, check to see if it's your own
+    // comment, and if yes, add the CID to your account comments database
+    for (const comment of flattenedComments) {
+      accountsStore
+        .getState()
+        .accountsActionsInternal.addCidToAccountComment(comment)
+        .catch((error: unknown) => log.error('repliesPagesStore.addNextRepliesPageToStore addCidToAccountComment error', {comment, error}))
+    }
+  },
+
+  // comments contain preloaded pages, those page comments must be added separately
+  addRepliesPageCommentsToStore: (comment: Comment) => {
+    if (!comment.replies?.pages) {
+      return
+    }
+
+    // find new comments in the page
+    const flattenedComments = utils.flattenCommentsPages(comment.replies.pages)
+    const {comments} = getState()
+    let hasNewComments = false
+    const newComments: Comments = {}
+    for (const comment of flattenedComments) {
+      if (comment.cid && (comment.updatedAt || 0) > (comments[comment.cid]?.updatedAt || 0)) {
+        // don't clone the comment to save memory, comments remain a pointer to the page object
+        newComments[comment.cid] = comment
+        hasNewComments = true
+      }
+    }
+
+    if (!hasNewComments) {
+      return
+    }
+
+    setState(({comments}: any) => {
+      return {comments: {...comments, ...newComments}}
+    })
+    log('repliesPagesStore.addRepliesPageCommentsToStore', {comment, newComments})
+  },
+}))
+
+// TODO: handle pages clients states
+// set clients states on subplebbits store so the frontend can display it, dont persist in db because a reload cancels updating
+const onSubplebbitPostsClientsStateChange = (subplebbitAddress: string) => (clientState: string, clientType: string, sortType: string, clientUrl: string) => {
+  // subplebbitsStore.setState((state: SubplebbitsState) => {
+  //   const client = {state: clientState}
+  //   const subplebbit = {...state.subplebbits[subplebbitAddress]}
+  //   subplebbit.posts = {...subplebbit.posts}
+  //   subplebbit.posts.clients = {...subplebbit.posts.clients}
+  //   subplebbit.posts.clients[clientType] = {...subplebbit.posts.clients[clientType]}
+  //   subplebbit.posts.clients[clientType][sortType] = {...subplebbit.posts.clients[clientType][sortType]}
+  //   subplebbit.posts.clients[clientType][sortType][clientUrl] = client
+  //   return {subplebbits: {...state.subplebbits, [subplebbit.address]: subplebbit}}
+  // })
+}
+
+const subplebbitPostsClientsOnStateChange = (clients: any, onStateChange: Function) => {
+  // for (const sortType in clients?.ipfsGateways) {
+  //   for (const clientUrl in clients?.ipfsGateways?.[sortType]) {
+  //     clients?.ipfsGateways?.[sortType]?.[clientUrl].on('statechange', (state: string) => onStateChange(state, 'ipfsGateways', sortType, clientUrl))
+  //   }
+  // }
+  // for (const sortType in clients?.ipfsClients) {
+  //   for (const clientUrl in clients?.ipfsClients?.[sortType]) {
+  //     clients?.ipfsClients?.[sortType]?.[clientUrl].on('statechange', (state: string) => onStateChange(state, 'ipfsClients', sortType, clientUrl))
+  //   }
+  // }
+  // for (const sortType in clients?.plebbitRpcClients) {
+  //   for (const clientUrl in clients?.plebbitRpcClients?.[sortType]) {
+  //     clients?.plebbitRpcClients?.[sortType]?.[clientUrl].on('statechange', (state: string) => onStateChange(state, 'plebbitRpcClients', sortType, clientUrl))
+  //   }
+  // }
+}
+
+let fetchPagePending: {[key: string]: boolean} = {}
+const fetchPage = async (pageCid: string, subplebbitAddress: string, account: Account) => {
+  // subplebbit page is cached
+  const cachedSubplebbitPage = await repliesPagesDatabase.getItem(pageCid)
+  if (cachedSubplebbitPage) {
+    return cachedSubplebbitPage
+  }
+  const subplebbit = await account.plebbit.createSubplebbit({address: subplebbitAddress})
+
+  // set clients states on subplebbits store so the frontend can display it
+  subplebbitPostsClientsOnStateChange(subplebbit.posts?.clients, onSubplebbitPostsClientsStateChange(subplebbit.address))
+
+  const onError = (error: any) => log.error(`repliesPagesStore subplebbit '${subplebbitAddress}' failed subplebbit.posts.getPage page cid '${pageCid}':`, error)
+  const fetchedSubplebbitPage = await utils.retryInfinity(() => subplebbit.posts.getPage(pageCid), {onError})
+  await repliesPagesDatabase.setItem(pageCid, utils.clone(fetchedSubplebbitPage))
+  return fetchedSubplebbitPage
+}
+
+/**
+ * Util function to get all pages in the store for a
+ * specific comment+sortType using `RepliesPage.nextCid`
+ */
+export const getRepliesPages = (comment: Comment, sortType: string, repliesPages: RepliesPages) => {
+  assert(repliesPages && typeof repliesPages === 'object', `getRepliesPages repliesPages '${repliesPages}' invalid`)
+  const pages: SubplebbitPage[] = []
+  const firstPageCid = getRepliesFirstPageCid(comment, sortType)
+  // comment has no pages
+  // TODO: if a loaded comment doesn't have a first page, it's unclear what we should do
+  // should we try to use another sort type by default, like 'best', or should we just ignore it?
+  // 'return pages' to ignore it for now
+  if (!firstPageCid) {
+    return pages
+  }
+  const firstPage = repliesPages[firstPageCid]
+  if (!firstPage) {
+    return pages
+  }
+  pages.push(firstPage)
+  while (true) {
+    const nextCid = pages[pages.length - 1]?.nextCid
+    const repliesPage = nextCid && repliesPages[nextCid]
+    if (!repliesPage) {
+      return pages
+    }
+    pages.push(repliesPage)
+  }
+}
+
+export const getRepliesFirstPageCid = (comment: Comment, sortType: string) => {
+  assert(comment?.cid, `getRepliesFirstPageCid comment '${comment}' invalid`)
+  assert(sortType && typeof sortType === 'string', `getRepliesFirstPageCid sortType '${sortType}' invalid`)
+  // comment has preloaded replies for sort type
+  if (comment.replies?.pages?.[sortType]?.comments) {
+    return comment.replies?.pages?.[sortType]?.nextCid
+  }
+  return comment.replies?.pageCids?.[sortType]
+
+  // TODO: if a loaded comment doesn't have a first page, it's unclear what we should do
+  // should we try to use another sort type by default, like 'best', or should we just ignore it?
+}
+
+// reset store in between tests
+const originalState = repliesPagesStore.getState()
+// async function because some stores have async init
+export const resetRepliesPagesStore = async () => {
+  fetchPagePending = {}
+  // remove all event listeners
+  listeners.forEach((listener: any) => listener.removeAllListeners())
+  // destroy all component subscriptions to the store
+  repliesPagesStore.destroy()
+  // restore original state
+  repliesPagesStore.setState(originalState)
+}
+
+// reset database and store in between tests
+export const resetRepliesPagesDatabaseAndStore = async () => {
+  await localForageLru.createInstance({name: 'repliesPages'}).clear()
+  await resetRepliesPagesStore()
+}
+
+export default repliesPagesStore
