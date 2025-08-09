16b4ee5050061851b6d17aab242dc34922fb03b2
plebeius <tom@plebbit.com>
2025-08-09 19:40:02 +0200

    add fetch API parity in shim

--- config/node-fetch-browser-shim.js
+++ config/node-fetch-browser-shim.js
@@ -1,1 +1,12 @@
-export default (url, options) => fetch(url, options)
+const fetchShim = (url, options) => fetch(url, options)
+
+// Attach standard classes for parity with node-fetch API when code accesses fetch.Headers etc.
+fetchShim.Headers = globalThis.Headers
+fetchShim.Request = globalThis.Request
+fetchShim.Response = globalThis.Response
+
+export const Headers = globalThis.Headers
+export const Request = globalThis.Request
+export const Response = globalThis.Response
+
+export default fetchShim
